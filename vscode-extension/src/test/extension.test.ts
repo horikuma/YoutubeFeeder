dab68038ff6db7e0b97b5d5e38bc544d1b19ab21
horikuma <47321595+horikuma@users.noreply.github.com>
2026-04-19 08:59:51 +0900

    16: コマンド実行時の1000行表示を検証する

--- vscode-extension/src/test/extension.test.ts
+++ vscode-extension/src/test/extension.test.ts
@@ -3,13 +3,30 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+
+import { runPipeline } from '../pipeline';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('Command pipeline renders 1000 fixed virtual text lines', () => {
+		let decorations: readonly vscode.DecorationOptions[] | undefined;
+		const editor = {
+			setDecorations(_decorationType: vscode.TextEditorDecorationType, nextDecorations: readonly vscode.DecorationOptions[]) {
+				decorations = nextDecorations;
+			},
+		} as vscode.TextEditor;
+
+		runPipeline(editor);
+
+		assert.ok(decorations);
+		assert.strictEqual(decorations?.length, 1000);
+		assert.ok(
+			decorations?.every((decoration, index) =>
+				decoration.range.start.line === index
+				&& decoration.range.end.line === index
+				&& decoration.renderOptions?.before?.contentText === `line ${index + 1}`,
+			),
+		);
 	});
 });
